e1da0d29c58bd6500474fcfb88a13a33f9f42df0
Seth Galyon <32885187+sethgalyon@users.noreply.github.com>
2022-06-19 21:53:24 -0400

    Use date taken instead of date created
    The script was using the file system's date created instead of the photo's date taken. The script now looks at the exif data of the photo to determine the date taken. If no exif data exists or the file isn't TIFF, JPEG, PNG, Webp, or HEIC, the script uses the folder structure as the date with the day and time set to 0. For instance, 2004/11/photo.bmp will become 2004/11/20041100_1.bmp.

--- photo_renamer.py
+++ photo_renamer.py
@@ -1,9 +1,15 @@
 import os
 import time
+import exifread
 
 
 def photo_renamer(photos_dir):
-    entities = os.listdir(photos_dir)
+    try:
+        entities = os.listdir(photos_dir)
+    except FileNotFoundError:
+        print('\nError: Directory not found')
+        return
+
     dir_list = []
     for i, entity in enumerate(entities):
         if os.path.isdir(os.path.join(photos_dir, entity)):
@@ -15,37 +21,47 @@ def photo_renamer(photos_dir):
     for dir in dir_list:
         entities.remove(dir)
 
-    entities.sort(key=os.path.getctime)
+    files = []
+    for file in entities:
+        with open(file, 'rb') as image:
+            exif = exifread.process_file(image, details=False, stop_tag='DateTimeOriginal')
+        
+        try:
+            date = str(exif['EXIF DateTimeOriginal'])
+        except KeyError:
+            date = file.split('\\')
+            if len(date[-2]) <= 2:
+                date = f'{date[-3]}:{date[-2]}:00 00:00:00'
+            else:
+                date = f'{date[-2]}:00:00 00:00:00'
+        
+        date = ''.join(date.split(' ', 1)[0].split(':', 2) + date.split(' ', 1)[1].split(':', 2))
+
+        files.append((date, file))
+    
+    files.sort()
     
     num_photos_per_day = {}
-    for file in entities:
-        t = time.gmtime(os.path.getctime(file))
-        
-        year = str(t.tm_year)
-        
-        month = str(t.tm_mon)
-        if len(month) < 2:
-            month = '0' + month
-            
-        day = str(t.tm_mday)
-        if len(day) < 2:
-            day = '0' + day
-        
+    for file in files:
+        year = file[0][:4]
+        month = file[0][4:6]
+        day = file[0][6:8]
         date = year + month + day
         
-        newName = photos_dir + "\\" + date + "_" + str(num_photos_per_day.get(date, 1)) + ".jpg"
-        oldName = photos_dir + "\\" + file.split("\\")[-1]
+        newName = photos_dir + '\\' + date + '_' + str(num_photos_per_day.get(date, 1)) + '.' + file[1].split('\\')[-1].split('.')[-1]
+        oldName = photos_dir + '\\' + file[1].split('\\')[-1]
 
         os.rename(oldName, newName)
+        
         num_photos_per_day[date] = num_photos_per_day.get(date, 1) + 1
         
-    print(f"Folder, {photos_dir}, done!")
+    print(f'Folder, {photos_dir}, done!')
 
 
 if __name__ == '__main__':
     script_path = os.path.abspath(__file__)
     script_dir = os.path.dirname(script_path)
-    photos_dir = str(input("Please enter the directory of your photos: "))
+    photos_dir = str(input('\nPlease enter the directory of your photos: '))
     photos_dir = os.path.join(script_dir, photos_dir)
     
     photo_renamer(photos_dir)
