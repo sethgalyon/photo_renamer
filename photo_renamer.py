4d6ab8af18b7bd1590489f116041ba81d8f0aa27
Seth Galyon <32885187+sethgalyon@users.noreply.github.com>
2022-06-20 11:31:38 -0400

    Increase time efficiency

--- photo_renamer.py
+++ photo_renamer.py
@@ -15,44 +15,38 @@ def photo_renamer(photos_dir):
             dir_list.append(entity)
             photo_renamer(os.path.join(photos_dir, entity))
         else:
-            entities[i] = os.path.join(photos_dir, entity)
+            file = os.path.join(photos_dir, entity)
+            with open(file, 'rb') as image:
+                exif = exifread.process_file(image, details=False, stop_tag='DateTimeOriginal')
+            
+            try:
+                date = str(exif['EXIF DateTimeOriginal'])
+            except KeyError:
+                date = file.split('\\')
+                if len(date[-2]) <= 2:
+                    date = f'{date[-3]}:{date[-2]}:00 00:00:00'
+                else:
+                    date = f'{date[-2]}:00:00 00:00:00'
+            
+            date = ''.join(date.split(' ', 1)[0].split(':', 2) + date.split(' ', 1)[1].split(':', 2))
+
+            entities[i] = (date, file)
             
     for dir in dir_list:
         entities.remove(dir)
 
-    files = []
-    for file in entities:
-        with open(file, 'rb') as image:
-            exif = exifread.process_file(image, details=False, stop_tag='DateTimeOriginal')
-        
-        try:
-            date = str(exif['EXIF DateTimeOriginal'])
-        except KeyError:
-            date = file.split('\\')
-            if len(date[-2]) <= 2:
-                date = f'{date[-3]}:{date[-2]}:00 00:00:00'
-            else:
-                date = f'{date[-2]}:00:00 00:00:00'
-        
-        date = ''.join(date.split(' ', 1)[0].split(':', 2) + date.split(' ', 1)[1].split(':', 2))
-
-        files.append((date, file))
+    entities.sort()
     
-    files.sort()
-    
-    num_photos_per_day = {}
-    for file in files:
-        year = file[0][:4]
-        month = file[0][4:6]
-        day = file[0][6:8]
-        date = year + month + day
+    photos_per_day = {}
+    for date, file in entities:
+        date = date[:4] + date[4:6] + date[6:8]
         
-        newName = photos_dir + '\\' + date + '_' + str(num_photos_per_day.get(date, 1)) + '.' + file[1].split('\\')[-1].split('.')[-1]
-        oldName = photos_dir + '\\' + file[1].split('\\')[-1]
+        newName = photos_dir + '\\' + date + '_' + str(photos_per_day.get(date, 1)) + '.' + file.split('\\')[-1].split('.')[-1]
+        oldName = photos_dir + '\\' + file.split('\\')[-1]
 
         os.rename(oldName, newName)
         
-        num_photos_per_day[date] = num_photos_per_day.get(date, 1) + 1
+        photos_per_day[date] = photos_per_day.get(date, 1) + 1
         
     print(f'Folder, {photos_dir}, done!')
 
